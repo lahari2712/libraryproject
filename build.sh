45f3d0421e8cc57ff7b8f26b17ab6d438aa4daec
Lahari <lahari@gmail.com>
2026-07-11 08:57:36 +0530

    Fix create_superuser script

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
 python manage.py migrate
+
+python manage.py collectstatic --no-input
+
 python create_superuser.py
