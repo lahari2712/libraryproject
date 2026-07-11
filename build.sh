bdd51135e921113881e316788207d69379f84423
Lahari <lahari@gmail.com>
2026-07-11 11:02:59 +0530

    Fix create superuser

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ python manage.py migrate
 
 python manage.py collectstatic --no-input
 
-python create_superuser.py
+python manage.py shell < create_superuser.py
--- create_superuser.py
+++ create_superuser.py
@@ -1,21 +1,15 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "libraryproject.settings")
-django.setup()
-
 from django.contrib.auth.models import User
 
 username = "admin"
+email = "admin@gmail.com"
 password = "admin123"
-email = "admin@example.com"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(
         username=username,
         email=email,
-        password=password
+        password=password,
     )
-    print("✅ Superuser created.")
+    print("Superuser created.")
 else:
-    print("✅ Superuser already exists.")
+    print("Superuser already exists.")
